c0c550e8be4ccebb6a1fd5d69ae4b578b6d34975
AleIHC <ale.shc218@gmail.com>
2025-10-03 09:42:13 -0300

    feat(backend): se agregan entidades y conexión a PostgreSQL

---
+++ backend/travelapp/src/main/java/com/travelapp/model/Destino.java
@@ -0,0 +1,32 @@
+package com.travelapp.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Destino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+    private String pais;
+    
+    @OneToMany(mappedBy = "destino", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Viaje> viajes;
+
+}
---
+++ backend/travelapp/src/main/java/com/travelapp/model/Viaje.java
@@ -0,0 +1,35 @@
+package com.travelapp.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Viaje {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Double precio;
+
+    @ManyToOne
+    @JoinColumn(name = "destino_id", nullable = false)
+    private Destino destino;
+
+    
+}
